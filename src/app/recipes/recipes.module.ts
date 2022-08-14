025a319db07748a0541df19e2bb2ed0603b6b324
Murtuza <murtuza.shaikh@hbwsl.com>
2022-08-14 21:14:09 +0530

    some changes

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -6,7 +6,6 @@ import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 
 import { NgModule } from "@angular/core";
-import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RecipeRoutingModule } from './recipe-routing.module';
